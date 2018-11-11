fdccb30467f00d7c746ff9a2bde494cae0b1f18c
Mathieu Santostefano <msantostefano@jolicode.com>
2018-11-11 12:35:55 +0100

    Reset d

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -4,8 +4,6 @@ echo ""
 echo ">>> POST BUILD SCRIPT"
 echo ""
 
-./bin/console doctrine:database:drop --force --if-exists
-./bin/console doctrine:database:create --if-not-exists
 ./bin/console doctrine:migrations:migrate -n
 ./bin/console doctrine:fixtures:load -n
 ./bin/console ckeditor:install -n --no-progress-bar
