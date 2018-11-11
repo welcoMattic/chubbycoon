c7b0b971bb905b7fa65d3a62fa7e77247254f682
Mathieu Santostefano <msantostefano@jolicode.com>
2018-11-11 12:21:43 +0100

    Reset db

--- clevercloud/post_build.sh
+++ clevercloud/post_build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+./bin/console doctrine:database:drop --force --if-exists
+./bin/console doctrine:database:create --if-not-exists
 ./bin/console doctrine:migrations:migrate -n
+./bin/console doctrine:fixtures:load -n
 ./bin/console ckeditor:install -n
 ./bin/console assets:install public
 
